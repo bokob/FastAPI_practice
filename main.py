ce50711a2ecfd41800818cab5b9f9056a9f3d42e
bokob <bmc0520@naver.com>
2023-01-15 19:31:34 +0900

    Query Parameter, Default value

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from enum import Enum
-
 from fastapi import FastAPI
 
+app =FastAPI()
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name":"Baz"}]
 
 
-app = FastAPI()
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/items/")
+async def read_item(skip: int = 0 , limit: int = 10):
+    return fake_items_db[skip:skip+limit]
