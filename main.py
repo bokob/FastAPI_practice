7c37d063879f6c28bb9b997f221a940ef8e4b74a
bokob <bmc0520@naver.com>
2023-01-16 13:48:21 +0900

    Query 파라미터 정규식으로 제한하기

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from typing import Optional
 
 from fastapi import FastAPI, Query
 
-app = FastAPI()
+app =FastAPI()
 
 @app.get("/items/")
-async def read_items(q: Optional[str]=Query(None, max_length=50)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(q:Optional[str]=Query(None, min_length=3, max_length=50, regex="^fixedquery$")):
+        results = {"items":[{"item_id":"Foo"}, {"item_id":"Bar"}]}
+        if q:
+            results.update({"q": q})
+        return results
