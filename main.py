55d60f0a60d0d9b922cf07134e63bd5fdb1ba0e8
bokob <bmc0520@naver.com>
2023-01-16 11:57:37 +0900

    Query 클래스 사용

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from typing import Optional
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-    
 app = FastAPI()
 
-@app.post("/items/{item_id}")
-async def create_item(item_id: int, item: Item, q: Optional[str] = None):
-    result = {"item_id": item_id, **item.dict()}
+@app.get("/items/")
+async def read_items(q: Optional[str]=Query(None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
