f18435f272aa256a73bfe52ed338ecc02175678d
bokob <bmc0520@naver.com>
2023-01-16 11:44:11 +0900

    모델 사용하기

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from fastapi import FastAPI
-
 from pydantic import BaseModel
 
 class Item(BaseModel):
@@ -9,9 +8,14 @@ class Item(BaseModel):
     description: Optional[str] = None
     price: float
     tax: Optional[float] = None
-
+    
 app = FastAPI()
 
 @app.post("/items/")
-async def create_item(item:Item):
-    return item
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    
+    return item_dict
